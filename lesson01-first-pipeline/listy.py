5054683fdce6214e38a85a59f7fff4309bdf8dd1
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-09-09 19:01:09 +0200

    feat: list examples

---
+++ lesson01-first-pipeline/listy.py
@@ -0,0 +1,23 @@
+lista = []
+print lista
+
+lista2 = [1, 2, 10000]
+print lista2
+
+lista3 = []
+lista3.append(1)
+lista3.append(2)
+lista3.append(3)
+print lista3
+
+print "Drugi element listy trzeciej: " + str(lista3[1])
+print "Drugi element listy trzeciej: " + str(lista3[1])
+
+macierz = [
+  [1, 2, 3],
+  [0, 1, 0],
+  [3, 3, 2]
+]
+print macierz
+
+print "Srodkowy element macierzy: " + str(macierz[1][1])
