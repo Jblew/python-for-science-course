528634e998da1010092dee73e0e46fee79f1d132
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2020-09-09 19:01:01 +0200

    feat: loop examples

---
+++ lesson01-first-pipeline/petle.py
@@ -0,0 +1,9 @@
+listA = [1, 2, 3, 100]
+
+for value in listA:
+  print 'Wartosc ' + str(value)
+
+listA = [1, 2, 3, 100]
+
+for index, value in enumerate(listA):
+  print 'Wartosc w miejscu ' + str(index) + ' wynosi ' + str(value)
